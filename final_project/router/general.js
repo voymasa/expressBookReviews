5f8252e3604c9d7c652a2318b0ba36f218dc14f2
avoymas01@gmail.com <avoymas01@gmail.com>
2024-09-02 17:59:42 -0400

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,9 +21,16 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get("/isbn/:isbn",function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  //let book = books.filter((book) => book.isbn === isbn);
+  let book = books[isbn];
+  return res.status(200).send(JSON.stringify(
+    book,
+    null,
+    4
+  ));
  });
   
 // Get book details based on author
