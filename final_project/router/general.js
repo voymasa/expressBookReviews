2e24dc7173813f22c8e30b2c23cb826b95a86225
avoymas01@gmail.com <avoymas01@gmail.com>
2024-09-05 00:45:47 -0400

    convert / to axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,30 +41,27 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', (req, res) => {
+public_users.get('/', async (req, res) => {
   //Write your code here
-  return res.status(200).send(JSON.stringify(
-    books,
-    null,
-    4
-  ));
-});
-
-// axios version of get book list
-const getBooksWithAxios = () => {
-  const req = axios.get('/');
+  const req = axios.get(`${BASE_URL}/`);
   req.then(resp => {
-    let bookList = resp.data;
-    return JSON.stringify(
-      bookList,
+    return resp.status(200).send(JSON.stringify(
+      books,
       null,
       4
-    );
+    ));
   })
   .catch(err => {
-    return err.toString();
+    return err.status(404).json({
+      message: "No books found"
+    });
   });
-}
+  // return res.status(200).send(JSON.stringify(
+  //   books,
+  //   null,
+  //   4
+  // ));
+});
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', (req, res) => {
